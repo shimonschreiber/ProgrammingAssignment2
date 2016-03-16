b9c2578fa6906341892214e313bf9f53789caf20
Shimon Schreiber <shimon.schreiber@gmail.com>
2016-03-16 14:38:44 +0200

    cachematrix allows creation of matrixes and caching their inverted values Usage: - makeMatrix - creates an empty matrix and the infrastructure - insert a matrix into the object using   <objectname>$set(matrix(parameters))   call <objectname>$get to get the matrix   call cacheSolve(objectname) to return the inverted matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## In this example we introduce the `<<-` operator which can be used to assign
+## a value to an object in an environment that is different from the current
+## environment. Below are two functions that are used to create a special
+## object that stores a numeric vector and caches its mean.
+##
+## The first function, `makeVector` creates a special "vector", which is a
+## list containing functions to
+##  1.  set the value of the matrix
+##  2.  get the value of the matrix
+##  3.  set the value of the inverted matrix
+##  4.  get the value of the inverted matrix
 
+makeMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvert <- function(inverted) m <<- inverted
+  getinvert <- function() m
+  list(set = set, get = get,
+       setinvert = setinvert,
+       getinvert = getinvert)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinvert()
+  if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvert(m)
+  m
 }
